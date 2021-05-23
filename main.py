d65c47e99c2fd2fa8ada23053ae8c8eab9f719b7
skaftisveins <skaftisveins@gmail.com>
2021-05-23 16:02:33 +0000

    Added environment variables and minor changes.

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ import requests
 from twilio.rest import Client
 
 weather_params = {
-    "lat": MY_LAT,
-    "lon": MY_LONG,
-    "appid": API_KEY,
+    "lat": my_lat,
+    "lon": my_long,
+    "appid": owm_api_key,
     "exclude": "current,minutely,daily"
 }
 
-response = requests.get(OWM_ENDPOINT, weather_params)
+response = requests.get("https://api.openweathermap.org/data/2.5/onecall", weather_params)
 response.raise_for_status()
 
 print(f"HTTP Status Code: {response.status_code}")
 
 weather_data = response.json()
-weather_slice = weather_data["hourly"][:12]
 
 weather_cond = ["Rain" if weather_data["hourly"][i]
                 ["weather"][0]["id"] < 700 else "Dry" for i in range(12)]
 
 if "Rain" in weather_cond:
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client = Client(twilio_account_sid, twilio_auth_token)
     message = client.messages \
         .create(
-            body="It's going to rain today. Remember to bring an ☔",
-            from_=PHONE_NUMBER_01,
-            to=PHONE_NUMBER_02
+            body="It's going to rain today. Remember to bring an umbrella☔",
+            from_=twilio_phone_number,
+            to=my_phone_number
         )
     print(message.status)
+    
+else:
+    print("It looks like it's not raining today, for the next 12 hours at least..")
